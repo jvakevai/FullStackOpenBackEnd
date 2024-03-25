c024b95099a5ad79603bfaa3058c5c8f20233bb3
Johannes Väkeväinen <johannes.vakevainen@student.oulu.fi>
2024-03-25 13:16:16 +0200

    changed port

--- persons.js
+++ persons.js
@@ -84,7 +84,7 @@ app.post('/api/persons', (request, response) => {
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :person'))
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
